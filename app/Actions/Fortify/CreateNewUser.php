d4beb8a235b676e67930f8c81c75a6b582da16e6
izulistiyan@gmail.com <izulistiyan@gmail.com>
2023-04-12 21:14:35 +0700

    update bug welcome

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -29,7 +29,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
-            'role' =>  $input['role'],
+            'role' =>  0,
             'nim' =>  $input['nim'],
             'password' => Hash::make($input['password']),
         ]);
